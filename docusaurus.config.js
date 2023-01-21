2abb091da76b9f9ed90597dc17a352fdeb398676
Saúl Domínguez Gómez <saul@MacBook-Pro-de-Saul.local>
2023-01-21 23:17:15 +0100

    feat: add CNAME file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
     title: 'RedditShot',
     tagline: `An API to generate beautiful images from the Reddit posts that you want.`,
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://redditshot.com',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
